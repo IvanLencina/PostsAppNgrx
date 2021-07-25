ff2f252427fccbc6b3232f1f433bd8007c5ed151
Ivan Lencina <ilencina@xubio.com>
2021-07-25 20:36:09 -0300

    Post Actions created.

---
+++ src/app/core/actions/posts/actions.ts
@@ -0,0 +1,14 @@
+
+export enum PostActionTypes {
+  ADD_POST = '[POST] Add Post',
+  ADD_POST_SUCCESS = '[POST] Add Post Success',
+  ADD_POST_FAIL = '[POST] Add Post Fail',
+
+  GET_POST = '[POST] Get Post',
+  GET_POST_SUCCESS = '[POST] Get Post Success',
+  GET_POST_FAIL = '[POST] Get Post Fail',
+
+  DELETE_POST = '[POST] Delete Post',
+  DELETE_POST_SUCCESS = '[POST] Delete Post Success',
+  DELETE_POST_FAIL = '[POST] Delete Post Fail',
+}
