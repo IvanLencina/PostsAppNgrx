60de71f791cafb3acced27b543dafe53e3014aab
Ivan Lencina <ilencina@xubio.com>
2021-07-25 21:00:17 -0300

    Post reducer created.

--- src/app/core/reducers/posts/reducer.ts
+++ src/app/core/reducers/posts/reducer.ts
@@ -1,3 +1,4 @@
+import { PostAction, PostActionTypes } from './../../actions/posts/actions';
 import Post from '../../models/post-model';
 
 export interface PostState {
@@ -22,7 +23,67 @@ const initialState: PostState = {
     },
   ],
   loading: false,
-  error: ''
+  error: '',
 };
 
+export function PostsReducer(state: PostState = initialState, action: PostAction) {
+  switch (action.type) {
+    case PostActionTypes.ADD_POST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case PostActionTypes.ADD_POST_SUCCESS:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts],
+        loading: false,
+      };
+    case PostActionTypes.ADD_POST_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    case PostActionTypes.DELETE_POST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case PostActionTypes.DELETE_POST_SUCCESS: {
+      let updatedPosts = [...state.posts];
+      updatedPosts.splice(action.payload, 1);
 
+      return {
+        ...state,
+        posts: updatedPosts,
+        loading: false,
+      };
+    }
+    case PostActionTypes.DELETE_POST_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    case PostActionTypes.GET_POSTS:
+      return {
+        ...state,
+        loading: true,
+      };
+    case PostActionTypes.GET_POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false,
+      };
+    case PostActionTypes.GET_POSTS_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
