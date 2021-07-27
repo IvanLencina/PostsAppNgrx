e64aa577b806ed23b14bc306c2db4dfda70dfcd3
Ivan Lencina <ilencina@xubio.com>
2021-07-26 21:31:20 -0300

    loading logic implemented.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
+import AppState from './core/models/app-state.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PostsAppNgrx';
+
+  loading$: Observable<boolean>;
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit(): void {
+    this.loading$ = this.store.select(store => store.post.loading);
+  }
 }
